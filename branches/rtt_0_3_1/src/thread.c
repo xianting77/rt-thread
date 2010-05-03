8e28f17f1ebbcdab87406b361994801190cf08fc
bernard.xiong <bernard.xiong@bbd45198-f89e-11dd-88c7-29a3b14d5316>
2010-05-03 23:14:59 +0000

    remove rt_object_find function.

--- branches/rtt_0_3_1/src/thread.c
+++ branches/rtt_0_3_1/src/thread.c
@@ -1,7 +1,7 @@
 /*
  * File      : thread.c
  * This file is part of RT-Thread RTOS
- * COPYRIGHT (C) 2006 - 2009, RT-Thread Development Team
+ * COPYRIGHT (C) 2006 - 2010, RT-Thread Development Team
  *
  * The license and distribution terms for this file may be
  * found in the file LICENSE in this distribution or at
@@ -620,11 +620,37 @@ void rt_thread_timeout(void* parameter)
  */
 rt_thread_t rt_thread_find(char* name)
 {
-	struct rt_thread* thread;
+	struct rt_object_information *information;
+	struct rt_object* object;
+	struct rt_list_node* node;
 
-	thread = (struct rt_thread*)rt_object_find(RT_Object_Class_Thread, name);
+	extern struct rt_object_information rt_object_container[];
 
-	return thread;
+	/* enter critical */
+	if (rt_thread_self() != RT_NULL)
+		rt_enter_critical();
+
+	/* try to find device object */
+	information = &rt_object_container[RT_Object_Class_Thread];
+	for (node = information->object_list.next; node != &(information->object_list); node = node->next)
+	{
+		object = rt_list_entry(node, struct rt_object, list);
+		if (rt_strncmp(object->name, name, RT_NAME_MAX) == 0)
+		{
+			/* leave critical */
+			if (rt_thread_self() != RT_NULL)
+				rt_exit_critical();
+
+			return (rt_thread_t)object;
+		}
+	}
+
+	/* leave critical */
+	if (rt_thread_self() != RT_NULL)
+		rt_exit_critical();
+
+	/* not found */
+	return RT_NULL;
 }
 
 /*@}*/
