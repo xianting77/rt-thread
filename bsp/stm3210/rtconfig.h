04e76fc451e8943546b36ec5f447a4feb72b1d0a
bernard.xiong <bernard.xiong@bbd45198-f89e-11dd-88c7-29a3b14d5316>
2011-04-12 07:22:39 +0000

    update configuration.

--- bsp/stm3210/rtconfig.h
+++ bsp/stm3210/rtconfig.h
@@ -73,7 +73,7 @@
 #define FINSH_USING_DESCRIPTION
 
 /* SECTION: device filesystem */
-#define RT_USING_DFS
+/* #define RT_USING_DFS */
 #define RT_USING_DFS_ELMFAT
 
 /* the max number of mounted filesystem */
@@ -84,7 +84,7 @@
 #define DFS_CACHE_MAX_NUM   		4
 
 /* SECTION: lwip, a lighwight TCP/IP protocol stack */
-#define RT_USING_LWIP
+/* #define RT_USING_LWIP */
 /* LwIP uses RT-Thread Memory Management */
 #define RT_LWIP_USING_RT_MEM
 /* Enable ICMP protocol*/
