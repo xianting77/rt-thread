2cb4bf5d52013b3b84526833f32af1fdaeade2a6
bernard.xiong@gmail.com <bernard.xiong@gmail.com@bbd45198-f89e-11dd-88c7-29a3b14d5316>
2013-05-24 22:45:34 +0000

    Add more result checking in DFS; Remove isalpha function by using is_alpha macro.

--- trunk/components/dfs/src/dfs.c
+++ trunk/components/dfs/src/dfs.c
@@ -304,18 +304,24 @@ char *dfs_normalize_path(const char *directory, const char *filename)
     {
         fullpath = rt_malloc(strlen(directory) + strlen(filename) + 2);
 
+        if (fullpath == RT_NULL)
+            return RT_NULL;
+
         /* join path and file name */
-        rt_snprintf(fullpath, strlen(directory) + strlen(filename) + 2, 
+        rt_snprintf(fullpath, strlen(directory) + strlen(filename) + 2,
             "%s/%s", directory, filename);
     }
     else
     {
         fullpath = rt_strdup(filename); /* copy string */
+
+        if (fullpath == RT_NULL)
+            return RT_NULL;
     }
 
     src = fullpath;
     dst = fullpath;
-    
+
     dst0 = dst;
     while (1)
     {
